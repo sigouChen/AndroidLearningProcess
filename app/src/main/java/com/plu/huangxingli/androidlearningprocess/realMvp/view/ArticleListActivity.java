1ea952f092355b494ca98c0bdef04eca92cd230f
happycodinggirl <hxlily86@gmail.com>
2016-03-11 23:01:33 +0800

    实现一个真正的mvp模式

--- app/src/main/java/com/plu/huangxingli/androidlearningprocess/realMvp/view/ArticleListActivity.java
+++ app/src/main/java/com/plu/huangxingli/androidlearningprocess/realMvp/view/ArticleListActivity.java
@@ -42,7 +42,7 @@ public class ArticleListActivity extends AppCompatActivity implements ArticleVie
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace dwith your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
